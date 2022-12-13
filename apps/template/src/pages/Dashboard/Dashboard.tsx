15689f496240428d9d8758c75787d063574b3fbf
Denys <denys.bobrov@extrawest.com>
2022-12-13 18:04:10 +0200

    fix: add default template

---
+++ apps/template/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { MaterialDashboardContainer } from "@mono-redux-starter/tamplateapp";
+
+export const Dashboard: FC = () =>  (
+	<MaterialDashboardContainer />
+);
+
+export default Dashboard;
---
+++ apps/template/src/pages/Login/Login.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { MaterialLoginContainer } from "@mono-redux-starter/tamplateapp";
+
+export const Login: FC = () => (
+	<MaterialLoginContainer />
+);
+
+export default Login;

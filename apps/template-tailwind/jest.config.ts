5df0e26417e078b3799265782f3d77f906ad53f7
Denys <denys.bobrov@extrawest.com>
2022-12-13 18:06:17 +0200

    fix: update jest config

--- apps/template-tailwind/jest.config.ts
+++ apps/template-tailwind/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'template',
+  displayName: 'template-tailwind',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/template',
+  coverageDirectory: '../../coverage/apps/template-tailwind',
 };

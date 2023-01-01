fe14c992207c7000bb3a1fe661814ed31a01e4d0
Denys <denys.bobrov@extrawest.com>
2023-01-01 17:52:23 +0200

    fix: update menu item component

--- libs/tamplateapp/src/lib/components/common/MenuItem/MenuItem.tsx
+++ libs/tamplateapp/src/lib/components/common/MenuItem/MenuItem.tsx
@@ -1,16 +1,23 @@
 import { FC } from "react";
-import { Link, MenuItem as MuiMenuItem } from "@mui/material";
+import {
+	Link,
+	MenuItem as MuiMenuItem,
+	SvgIcon
+} from "@mui/material";
 import { NavLink as RouterLink } from "react-router-dom";
 import { menuItemStyles } from "./MenuItem.styles";
 import { MenuItemProps } from "./MenuItem.types";
 
-export const MenuItem: FC<MenuItemProps> = ({ href, title}) => (
-	<MuiMenuItem key={`menu${title}`}>
+export const MenuItem: FC<MenuItemProps> = ({ href, title, icon}) => (
+	<MuiMenuItem
+		sx={menuItemStyles.itemWrap}
+		key={`menu${title}`}
+	>
+		<SvgIcon sx={menuItemStyles.svgIcon} component={icon} />
 		<Link
 			component={RouterLink}
 			to={href}
-			sx={{...menuItemStyles.menuLink,
-				...menuItemStyles.wrap}}
+			sx={{...menuItemStyles.menuLink}}
 		>
 				{title}
 		</Link>

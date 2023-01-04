e2edd56e7ea6aec2c40f13da44643416ba2822dc
Denys <denys.bobrov@extrawest.com>
2023-01-04 11:59:20 +0200

    fix: update containers imports

--- libs/tamplateapp/src/lib/containers/index.ts
+++ libs/tamplateapp/src/lib/containers/index.ts
@@ -3,3 +3,4 @@ export * from "./LoginContainer/LoginContainer";
 export * from "./NotFoundContainer/NotFoundContainer";
 export * from "./ClientsContainer/ClientsContainer";
 export * from "./ForgotContainer/ForgotContainer";
+export * from "./RegistrationContainer/RegistrationContainer";

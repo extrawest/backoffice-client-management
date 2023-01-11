fac7c1afcf87ecec4a1e838be5c775b62fabb599
Denys <denys.bobrov@extrawest.com>
2023-01-11 03:09:49 +0200

    feat: add conver data functionality

---
+++ libs/firebase/src/lib/utils/converData.ts
@@ -0,0 +1,10 @@
+import { DocumentData, QueryDocumentSnapshot } from "firebase/firestore";
+
+export const convertData = (data: QueryDocumentSnapshot<unknown>[] | undefined) => {
+	if(!data){
+		return [];
+	}
+	return data.map((item: DocumentData) => {
+		return item.data();
+	});
+};

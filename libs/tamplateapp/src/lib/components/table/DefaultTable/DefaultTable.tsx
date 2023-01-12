0082c80905a289d16d2b3aa5392077f883050cf1
Denys <denys.bobrov@extrawest.com>
2023-01-12 04:27:21 +0200

    fix: update default table styles

--- libs/tamplateapp/src/lib/components/table/DefaultTable/DefaultTable.tsx
+++ libs/tamplateapp/src/lib/components/table/DefaultTable/DefaultTable.tsx
@@ -22,15 +22,29 @@ export const DefaultTable: FC<DefaultTableProps> = ({ rows, columns, handleSort,
         background: (theme: Theme) => theme.palette.background.default,
         minHeight: (theme: Theme) => theme.spacing(600/8),
 				border: "none",
+				"& .MuiDataGrid-row": {
+					maxHeight: "none !important",
+					padding: (theme: Theme) => theme.spacing(
+						15/8,
+						0
+					),
+					borderBottom: (theme: Theme) => `${theme.spacing(1/8)} solid ${theme.palette.grayscale[200]}`
+				},
         "& .MuiDataGrid-row:hover": {
           backgroundColor: "inherit",
           boxShadow: "0px -1.75px 4px rgba(33, 33, 52, 0.039), 0px 5.75px 10px rgba(33, 33, 52, 0.12)"
         },
+				"& .MuiDataGrid-columnHeader:focus": {
+					outline: "none"
+				},
         "& .MuiDataGrid-columnHeaderTitleContainer": {
           justifyContent: "space-between",
 					fontWeight: 700,
 					color: (theme: Theme) => theme.palette.grayscale[400]
         },
+				"& .MuiDataGrid-cell": {
+					borderBottom: "none"
+				},
         "& .MuiDataGrid-cell.ampTableCell:focus": {
           outline: "none"
         },

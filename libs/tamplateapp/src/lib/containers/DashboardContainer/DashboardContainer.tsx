2071573d8a742b3b6b688a2093f61619fb36ea11
Denys <denys.bobrov@extrawest.com>
2023-01-18 23:18:34 +0200

    feat: update dashboard container

--- libs/tamplateapp/src/lib/containers/DashboardContainer/DashboardContainer.tsx
+++ libs/tamplateapp/src/lib/containers/DashboardContainer/DashboardContainer.tsx
@@ -1,6 +1,60 @@
+import {
+	Box,
+	Typography,
+	Grid
+} from "@mui/material";
 import { FC } from "react";
-import DashboardField from "../../components/dashboard/DashboardField/DashboardField";
+import { FormattedMessage } from "react-intl";
+import { ChartWrapper } from "../../components/common/ChartWrapper/ChartWrapper";
+import { commonContainers } from "../commonContainers.styles";
 
 export const DashboardContainer: FC = () => (
-	<DashboardField />
+	<Box>
+		<Box sx={commonContainers["titleWrapperSx"]}>
+			<Typography
+				variant="h5"
+				sx={{ width: "calc(100% - 240px)" }}
+			>
+				<FormattedMessage id="template.dashboard" />
+			</Typography>
+		</Box>
+		<Grid
+			container
+			rowSpacing={4}
+			columnSpacing={2}
+		>
+			<Grid
+				item
+				xs={12}
+			>
+				<Box
+					sx={commonContainers["gridItem"]}
+				>
+					<ChartWrapper />
+				</Box>
+			</Grid>
+			<Grid
+				item
+				xs={12}
+				md={6}
+			>
+				<Box
+					sx={commonContainers["gridItem"]}
+				>
+					xs=6
+				</Box>
+			</Grid>
+			<Grid
+				item
+				xs={12}
+				md={6}
+			>
+				<Box
+					sx={commonContainers["gridItem"]}
+				>
+					xs=6
+				</Box>
+			</Grid>
+		</Grid>
+	</Box>
 );

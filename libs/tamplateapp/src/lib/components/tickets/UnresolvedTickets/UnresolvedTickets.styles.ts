ea9f77252a05fb7844975b9331e503b67bc226b3
Denys <denys.bobrov@extrawest.com>
2023-01-23 00:44:40 +0200

    feat:add unresolved tickets component

---
+++ libs/tamplateapp/src/lib/components/tickets/UnresolvedTickets/UnresolvedTickets.styles.ts
@@ -0,0 +1,26 @@
+import { Theme } from "@mui/material";
+import { SxStyles } from "../../../types/styles";
+
+export const unresolvedTicketsStyles: SxStyles = {
+	wrapper: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "space-between",
+		height: "100%"
+	},
+	ticketsWrapper: {
+		padding: (theme: Theme) => theme.spacing(
+			32/8,
+			32/8,
+			18/8
+		),
+		height: "100%",
+		marginBottom: 0,
+	},
+	titleWrapper: {
+		display: "flex",
+		flexDirection: "column",
+		gap: (theme: Theme) => theme.spacing(5/8),
+		width: "100%"
+	}
+};

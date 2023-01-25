d14ca9895b91ec8cd07de1fbea6f98ae33d4980b
Denys <denys.bobrov@extrawest.com>
2023-01-25 22:15:50 +0200

    fix: update main layout

--- libs/tailwind-lib/src/lib/layouts/MainLayout/MainLayout.tsx
+++ libs/tailwind-lib/src/lib/layouts/MainLayout/MainLayout.tsx
@@ -1,11 +1,11 @@
 import { FC, useState } from "react";
 import { NavLink as RouterLink } from "react-router-dom";
-import MenuIcon from "@mui/icons-material/Menu";
 import { AppRouteEnum } from "../../types/paths";
 import { MainLayoutProps } from "./MainLayout.types";
 import { mainLayoutStyles } from "./MainLayout.styles";
-import { IconButton } from "../../components/IconButton/IconButton";
-import { Link } from "../../components/Link/Link";
+import { IconButton } from "../../components/common/IconButton/IconButton";
+import { Link } from "../../components/common/Link/Link";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
 	const [open, setOpen] = useState(false);
@@ -18,23 +18,9 @@ export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
 		<div
 			className="flex overflow-hidden h-100vh"
 		>
-			<div
-				className="fixed z-2 xs:flex md:none"
-			>
-				<div>
-					<IconButton
-						onClick={toggleDrawer}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Link
-						to={AppRouteEnum.DASHBOARD}
-					>
-						Lagoon
-					</Link>
-				</div>
+			<div className="w-96 shadow-md shadow-secondary-main flex justify-center items-center">
+				<Sidebar />
 			</div>
-
 			<div
 				className="xs:mt-48 sm:mt-64 mt-0 p-0 w-100% overflow-x-hidden"
 				id="pageWrap"

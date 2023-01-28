a2711fcd618f966b686f08a60ded73f1eb72e964
Denys <denys.bobrov@extrawest.com>
2023-01-28 03:12:11 +0200

    feat: add cell with caption for tailwind

---
+++ libs/tailwind-lib/src/lib/components/common/CellWithCaption/CellWithCaption.tsx
@@ -0,0 +1,37 @@
+import { FC } from "react";
+import { FormattedMessage } from "react-intl";
+import { TypographyEnum } from "../../../types/typography";
+import { Typography } from "../Typography/Typography";
+import { CellWithCaptionProps } from "./CellWithCaption.types";
+
+export const CellWithCaption: FC<CellWithCaptionProps> = ({ mainText, captionFormatMessage, caption }) => {
+	return (
+		<div className="flex flex-col gap-1 p-2">
+			<Typography
+				extraClasses="font-semibold text-start"
+			>
+				{mainText}
+			</Typography>
+			{caption &&
+				<div
+					className="flex gap-1"
+				>
+					{captionFormatMessage &&
+						<Typography
+							type={TypographyEnum.DESCRIPTION}
+						>
+							<FormattedMessage
+								id={captionFormatMessage}
+							/>
+						</Typography>
+					}
+					<Typography
+						type={TypographyEnum.DESCRIPTION}
+					>
+						{caption}
+					</Typography>
+				</div>
+			}
+		</div>
+	);
+};

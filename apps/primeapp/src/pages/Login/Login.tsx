f978b9d4a1563bd5e86e0c930aeb08211b1095ec
Denys <denys.bobrov@extrawest.com>
2023-01-31 01:38:34 +0200

    fix: re-create prime app

---
+++ apps/primeapp/src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+import { LoginContainer } from "libs/primeapplib/src";
+import { FC } from "react";
+
+const Login: FC = () => {
+	return (
+		<div>
+			<LoginContainer />
+		</div>
+	);
+};
+
+export default Login;

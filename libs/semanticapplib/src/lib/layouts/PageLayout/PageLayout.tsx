306a4a3ad35123711180e19783dfc85c79889d61
Denys <denys.bobrov@extrawest.com>
2023-02-05 03:16:29 +0200

    feat: add page lyout & main layout

---
+++ libs/semanticapplib/src/lib/layouts/PageLayout/PageLayout.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import { PageLayoutProps } from "./Pagelayout.types";
+import { HelmetHead } from "./HelmetHead/HelmetHead";
+import { Container } from "semantic-ui-react";
+
+export const PageLayout: FC<PageLayoutProps> = ({ children }) =>  (
+	<Container
+		fluid
+		className="pageLayout"
+	>
+		<HelmetHead />
+		{children}
+	</Container>
+);
+
+export default PageLayout;

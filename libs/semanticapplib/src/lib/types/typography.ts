c32e32370355d5898177a152a9ecb6debffd19ad
Denys <denys.bobrov@extrawest.com>
2023-02-05 03:17:50 +0200

    feat: add default lib files

---
+++ libs/semanticapplib/src/lib/types/typography.ts
@@ -0,0 +1,23 @@
+export const enum TypographyEnum {
+	H1 = "h1",
+	H4 = "h4",
+	H5 = "h5",
+	BODY1 = "body1",
+	BODY2 = "body2",
+	BUTTON = "button",
+	SUBTITLE1 = "subtitle1",
+	DESCRIPTION = "description",
+	CAPTION = "caption",
+	TITLE = "title"
+}
+
+export const enum TagEnum {
+	P = "p",
+	H1 = "h1",
+	H2 = "h2",
+	H3 = "h3",
+	H4 = "h4",
+	H5 = "h5",
+	H6 = "h6",
+	SPAN = "span"
+}

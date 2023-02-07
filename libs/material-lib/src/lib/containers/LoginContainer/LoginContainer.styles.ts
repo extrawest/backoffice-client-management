048d2a59dac8beba8f3700cb2627bebee93d7979
Denys <denys.bobrov@extrawest.com>
2023-02-07 19:57:44 +0200

    feat: add google auth for mui

--- libs/material-lib/src/lib/containers/LoginContainer/LoginContainer.styles.ts
+++ libs/material-lib/src/lib/containers/LoginContainer/LoginContainer.styles.ts
@@ -2,6 +2,7 @@ export const styles = {
 	pageWrapper: {
 		height: "100vh",
 		display: "flex",
+		flexDirection: "column",
 		justifyContent: "center",
 		alignItems: "center",
 		backgroundColor: "background.paper"

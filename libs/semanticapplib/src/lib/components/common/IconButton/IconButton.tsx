30411b730a15a05b269a746afcc1885084ffeb99
Denys <denys.bobrov@extrawest.com>
2023-02-07 02:59:42 +0200

    feat: add icon button

---
+++ libs/semanticapplib/src/lib/components/common/IconButton/IconButton.tsx
@@ -0,0 +1,16 @@
+import { mergeStrings } from "@mono-redux-starter/shared/utils";
+import { FC } from "react";
+import { Button } from "semantic-ui-react";
+import { IconButtonProps } from "./IconButton.types";
+
+export const IconButton: FC<IconButtonProps> = ({
+	icon,
+	onClick,
+}) => {
+	return (
+		<Button
+			onClick={onClick}
+			icon={icon}
+		/>
+	);
+};

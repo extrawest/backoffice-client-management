5ab1b09097f6d0e7c84660c025e9a47fb6e68271
Denys <denys.bobrov@extrawest.com>
2023-02-07 16:55:19 +0200

    feat: add tickets form

---
+++ libs/semanticapplib/src/lib/forms/TicketForm/TicketForm.types.ts
@@ -0,0 +1,14 @@
+import { PriorityEnum, SemanticSelectValue } from "@mono-redux-starter/shared/types";
+import { FormikHelpers } from "formik";
+
+export type TicketValues = {
+	name: string,
+	client: string,
+	priority: string | PriorityEnum
+};
+
+export type TicketFormProps = {
+	isLoading: boolean;
+	onSubmit: (values: TicketValues, form: FormikHelpers<TicketValues>) => void;
+	processedClients: SemanticSelectValue[],
+};

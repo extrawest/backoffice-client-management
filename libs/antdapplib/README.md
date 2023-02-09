dac54f0a62a2419fa46194ff61a97cec053dde25
Denys <denys.bobrov@extrawest.com>
2023-02-09 16:50:57 +0200

    feat: init antd lib

---
+++ libs/antdapplib/README.md
@@ -0,0 +1,7 @@
+# antdapplib
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test antdapplib` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/antdapplib/src/lib/theme/index.ts
@@ -0,0 +1,32 @@
+import { ThemeConfig } from "antd/es/config-provider/context";
+import "@mono-redux-starter/shared/fonts";
+export const theme: ThemeConfig = {
+	token: {
+		fontFamily: "Open Sans, sans-serif",
+		colorPrimary: "#2F80ED",
+		colorPrimaryActive: "#F2994A",
+		colorError: "#EB5757",
+		colorSuccessBg: "#d3ffdc",
+		colorSuccess: "rgba(0, 255, 52, 0.674463)",
+		colorSuccessActive: "#2b822d",
+		colorFill: "#E6E6E6",
+		colorFillSecondary: "#D1D1D1",
+		colorFillTertiary: "#B9B9B9",
+		colorFillQuaternary: "#828282",
+		colorWarningBg: "#FFFBEE",
+		colorWarningBgHover: "#FFF7DE",
+		colorWarningBorder: "#FFF0BD",
+		colorWarningBorderHover: "#FFE89C",
+		colorWarningHover: "#FFE07B",
+		colorWarning: "#FFD85A",
+		colorWarningActive: "#FFD13D",
+		colorWarningTextHover: "#FFCA1F",
+		colorWarningText: "#FFC300",
+		colorWarningTextActive: "#E0AB00",
+		colorInfoBorder: "#56CCF2",
+		colorInfoHover: "#2D9CDB",
+		colorInfoActive: "#2F80ED",
+		colorInfoTextActive: "#1D2992",
+		colorInfoText: "#3751FF",
+	},
+};

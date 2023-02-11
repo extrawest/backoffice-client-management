a9cfd0eda786d638cd9bd6caec54bebf99b7c88b
Denys <denys.bobrov@extrawest.com>
2023-02-11 18:28:38 +0200

    feat: add image components

---
+++ libs/antdapplib/src/lib/components/common/ImageWrapper/ImageWrapper.tsx
@@ -0,0 +1,55 @@
+import {
+	Dispatch,
+	FC,
+	SetStateAction,
+	useEffect,
+	useState
+} from "react";
+import { ImageWrapperProps } from "./ImageWrapper.types";
+import { ref, getDownloadURL } from "firebase/storage";
+import { storage, useParseRef } from "@mono-redux-starter/firebase";
+import { Image } from "../Image/Image";
+
+const getFile = (
+	setImageSrc: Dispatch<SetStateAction<string | null>>,
+	name: string
+) => async () => {
+	const url = await getDownloadURL(ref(
+		storage,
+		`clients/${name}`
+	));
+	setImageSrc(url);
+};
+
+export const ImageWrapper: FC<ImageWrapperProps> = ({
+	reference,
+	rounded,
+	...rest
+}) => {
+
+	const [imageSrc, setImageSrc] = useState<string | null>(null);
+	const clientRef = useParseRef(reference);
+
+	useEffect(
+		() => {
+			if(!clientRef || !clientRef["image"]?.["id"]){
+				return;
+			}
+			getFile(
+				setImageSrc,
+				clientRef["image"]?.["id"]
+			)();
+		},
+		[reference, clientRef]
+	);
+
+	return (
+		<>
+			<Image
+				src={imageSrc ?? undefined}
+				rounded
+				{...rest}
+			/>
+		</>
+	);
+};

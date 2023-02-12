0dffc11591f96356f8cf41a9d66bf0ad3c2440c9
Denys <denys.bobrov@extrawest.com>
2023-02-13 01:19:44 +0200

    feat: add table component

---
+++ libs/antdapplib/src/lib/components/tickets/TicketsTable/TicketsTable.tsx
@@ -0,0 +1,71 @@
+
+import { Tickets } from "@mono-redux-starter/shared/types";
+import {
+	FC,
+	useEffect,
+	useState
+} from "react";
+import { useIntl } from "react-intl";
+import { TableAction } from "../../table/TableAction";
+import { FilterValue } from "../../table/TableAction/TableAction.types";
+import { TicketsTableProps } from "./TicketsTable.types";
+import {
+	Col,
+	Divider,
+	Row,
+	Table,
+	TableProps
+} from "antd";
+import { columnConfigUsers } from "./TicketsTable.tableConfig";
+import { SorterResult } from "antd/es/table/interface";
+
+export const TicketsTable: FC<TicketsTableProps> = ({
+	data,
+	currentPage,
+	totalElements,
+	limit,
+	handleUpdateTableData,
+	handleChangePage,
+}) => {
+	const intl = useIntl();
+
+	const [sortValue, setSortValue] = useState<SorterResult<Tickets>>({ columnKey: "", order: "ascend" });
+	const [filterValue, setFilterValue] = useState<FilterValue>({ priority: "" });
+	useEffect(
+		() => {
+			handleUpdateTableData({
+				...filterValue,
+				sortAsc: sortValue ? sortValue.order === "ascend" : true,
+				sortField: (sortValue && sortValue.columnKey) ?? ""
+			});
+		},
+		[sortValue, filterValue]
+	);
+
+	const handleChange: TableProps<Tickets>["onChange"] = (
+		pagination, filters, sorter
+	) => {
+		setSortValue(sorter as SorterResult<Tickets>);
+		handleChangePage(
+			pagination.current,
+			pagination.pageSize
+		);
+	};
+
+	return (
+    <Col>
+      <TableAction
+        handleFilter={( value: FilterValue ) => setFilterValue(value)}
+      />
+			<Divider
+				type="vertical"
+			/>
+			<Table
+				columns={columnConfigUsers}
+				dataSource={data}
+				size="middle"
+				onChange={handleChange}
+			/>
+    </Col>
+	);
+};
---
+++ libs/antdapplib/src/lib/components/tickets/TicketsTable/TicketsTable.types.ts
@@ -0,0 +1,10 @@
+import { Tickets, TicketsFilterDataType } from "@mono-redux-starter/shared/types";
+
+export interface TicketsTableProps {
+	data: Tickets[],
+	totalElements: number,
+	currentPage: number,
+	limit: number,
+	handleUpdateTableData: (value: TicketsFilterDataType) => void,
+	handleChangePage: (page?: number, size?: number) => void ,
+}

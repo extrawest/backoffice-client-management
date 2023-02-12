bfd3a992f414e9ada37eb52fe82e2a20755975c1
Denys <denys.bobrov@extrawest.com>
2023-02-12 20:46:41 +0200

    feat: add common form styles

---
+++ libs/antdapplib/src/lib/forms/commonForms.styles.ts
@@ -0,0 +1,9 @@
+import { CSSStyles } from "@mono-redux-starter/shared/types";
+import { pxToRem } from "@mono-redux-starter/shared/utils";
+
+export const formStyles: CSSStyles = {
+	form: {
+		width: "100%",
+		marginTop: pxToRem(10)
+	}
+};

a313d334811de396da81a681fafbb0a74fe62ab6
Denys <denys.bobrov@extrawest.com>
2023-02-13 01:18:13 +0200

    feat: add client form

---
+++ libs/antdapplib/src/lib/components/clients/ClientCreateFormWrapper/ClientCreateFormWrapper.tsx
@@ -0,0 +1,105 @@
+import {
+	collection,
+	doc,
+	setDoc,
+	Timestamp
+} from "@firebase/firestore";
+import { firestore, storage } from "@mono-redux-starter/firebase";
+import { CollectionEnum } from "@mono-redux-starter/shared/types";
+import { FC, useState } from "react";
+import { ClientCreateFormWrapperProps } from "./ClientCreateFormWrapper.types";
+import { useUploadFile } from "react-firebase-hooks/storage";
+import { ref } from "firebase/storage";
+import { Values } from "../../../forms/ClientForm/ClientForm.types";
+import ClientForm from "../../../forms/ClientForm/ClientForm";
+import DropZone from "../../common/DropZone";
+import { Col, Row } from "antd";
+import { pxToRem } from "@mono-redux-starter/shared/utils";
+
+type UploadFileHookType = ReturnType<typeof useUploadFile>[0];
+
+const handleSubmit = (
+	handleClose: () => void,
+	file: File | null,
+	uploadFile: UploadFileHookType
+) => async (values: Values) => {
+	const clientsRef = collection(
+		firestore(),
+		CollectionEnum.CLIENTS
+	);
+	try{
+		let imageReference;
+
+		if(file) {
+			const storageRef = ref(
+				storage,
+				`${CollectionEnum.CLIENTS}/${values.firstName}_${values.lastName}.jpg`
+			);
+			await uploadFile(
+				storageRef,
+				file,
+				{
+					contentType: "image/jpeg"
+				}
+			);
+			imageReference = storageRef;
+		}
+
+		const currentDate = (new Date()).getTime();
+
+		const data = {
+			firstName: values.firstName,
+			lastName: values.lastName,
+			email: values.email,
+			age: Timestamp.fromMillis((new Date(values.date).getTime())),
+			date: Timestamp.fromMillis(currentDate),
+			id:"id" + currentDate,
+			imageReference: imageReference ? `${imageReference.bucket}/${imageReference.fullPath}` : ""
+		};
+
+		await setDoc(
+			doc(clientsRef),
+			data
+		);
+	} catch (error) {
+		console.log(error);
+	} finally {
+		handleClose();
+	}
+};
+
+export const ClientCreateFormWrapper: FC<ClientCreateFormWrapperProps> = ({ handleClose }) => {
+	const [image, setImage] = useState<File | null>(null);
+	const [uploadFile, uploading, snapshot, error] = useUploadFile();
+
+	const handleFile = (file: File | null) => {
+		file && setImage(file);
+	};
+
+	return (
+		<Row
+			align="top"
+			justify="space-between"
+			style={{
+				marginTop: pxToRem(20)
+			}}
+		>
+			<Col span={7}>
+				<DropZone
+					handleFile={handleFile}
+				/>
+			</Col>
+			<Col span={16}>
+				<ClientForm
+					onSubmit={
+						handleSubmit(
+							handleClose,
+							image,
+							uploadFile
+						)}
+					isLoading={false}
+				/>
+			</Col>
+		</Row>
+	);
+};
---
+++ libs/antdapplib/src/lib/forms/ClientForm/ClientForm.types.ts
@@ -0,0 +1,14 @@
+import { Dayjs } from "dayjs";
+import { FormikHelpers } from "formik";
+
+export type Values = {
+	firstName: string,
+	email: string,
+	lastName: string,
+	date: number | string
+};
+
+export type ClientFormProps = {
+	isLoading: boolean;
+	onSubmit: (values: Values) => void;
+};
